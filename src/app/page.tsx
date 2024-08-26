7346b55bf7980becfc369eab686ab468189ccebf
lolasanchezz <lolansanchez@icloud.com>
2024-08-26 01:38:13 -0400

    left basic side done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+'use client'
+
 import {Name} from "./components/Name"
 import styles from "./page.module.css";
+import {useState} from 'react';
+
+
 
 export default function Home() {
+  const [info, setInfo] = useState('');
+
+
+
   return (
     <main className={styles.main}>
-      <Name></Name>
+      <Name setInfo = {setInfo}></Name>
     </main>
   );
 }
